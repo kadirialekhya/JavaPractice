ffff3c78425f57890ef4dd53e5525c8c20075aa9
Alekhya Kadiri <kadiri.a@northeastern.edu>
2022-02-27 08:00:18 -0500

    String Reverse without Fun

--- src/FullTimePrep/TopJavaQuestions/StringReverseWithoutFun.java
+++ src/FullTimePrep/TopJavaQuestions/StringReverseWithoutFun.java
@@ -1,2 +1,12 @@
-package FullTimePrep.TopJavaQuestions;public class StringReverseWithoutFun {
+package FullTimePrep.TopJavaQuestions;
+
+public class StringReverseWithoutFun {
+    public static void main(String[] args) {
+        String str="alekhya";
+        char[] ch =str.toCharArray();
+        for (int i=ch.length-1;i>=0;i--)
+        {
+            System.out.println(ch[i]);
+        }
+    }
 }
