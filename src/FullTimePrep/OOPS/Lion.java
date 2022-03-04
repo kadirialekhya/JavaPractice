0cd6c501385e8bd2958f2ca7bd3e082eb5a66698
Alekhya Kadiri <kadiri.a@northeastern.edu>
2022-03-04 10:14:42 -0500

    Interface-one

--- src/FullTimePrep/OOPS/Lion.java
+++ src/FullTimePrep/OOPS/Lion.java
@@ -1,2 +1,14 @@
-package FullTimePrep.OOPS;public class Lion {
-}
+package FullTimePrep.OOPS;
+
+public class Lion implements Animal {
+    @Override
+        public void habitat()
+        {
+            System.out.println("habitat is land");
+        }
+        @Override
+    public void food(){
+        System.out.println("Lion eat - flesh");
+    }
+    }
+
