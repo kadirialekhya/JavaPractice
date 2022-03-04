6de99283c18df04fb2a22b8c692463a27baf9d31
Alekhya Kadiri <kadiri.a@northeastern.edu>
2022-03-04 10:15:43 -0500

    Interface-three

--- src/FullTimePrep/OOPS/MyClass.java
+++ src/FullTimePrep/OOPS/MyClass.java
@@ -1,2 +1,13 @@
-package FullTimePrep.OOPS;public class MyClass {
+package FullTimePrep.OOPS;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Animal lion=new Lion();
+        lion.habitat();
+        lion.food();
+        System.out.println();
+        Animal whale=new Whale();
+        whale.food();
+        whale.habitat();
+    }
 }
