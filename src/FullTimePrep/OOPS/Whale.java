0012c6cd5f2ea07add853bf7bfa5301bd0220f2f
Alekhya Kadiri <kadiri.a@northeastern.edu>
2022-03-04 10:15:11 -0500

    Interface-two

--- src/FullTimePrep/OOPS/Whale.java
+++ src/FullTimePrep/OOPS/Whale.java
@@ -1,2 +1,14 @@
-package FullTimePrep.OOPS;public class Whale {
+package FullTimePrep.OOPS;
+
+public class Whale implements Animal{
+
+    @Override
+    public void habitat() {
+        System.out.println("water");
+    }
+
+    @Override
+    public void food() {
+        System.out.println("insects");
+    }
 }
